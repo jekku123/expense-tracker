54879234d4ee7a6cc9f390e149e05ec491d096fa
Jesse Manninen <jesse.manninen@gmail.com>
2024-03-16 01:33:32 +0200

    add aws to cos

--- server/src/config/corsOptions.ts
+++ server/src/config/corsOptions.ts
@@ -1,4 +1,8 @@
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:3000'];
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:3000',
+  'http://13.48.129.24:5173',
+];
 
 export const corsOptions = {
   origin: function (
