6a44f3e731ba76d6f5c0b58cf721baf255bdf9ca
Jesse Manninen <jesse.manninen@gmail.com>
2024-03-16 17:29:52 +0200

    jwt token to secure

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -28,7 +28,7 @@ export class AuthController {
 
       res.cookie('jwt', refreshToken, {
         httpOnly: true,
-        secure: false,
+        secure: true,
         sameSite: 'none',
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
